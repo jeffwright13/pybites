195f364680af3819f5ec9cfccc654347522225d1
Jeff Wright <Jeff.Wright@gmail.com>
2021-05-13 09:59:06 -0400

    Non-working code and tests for Bite21.

--- regular_bites/bite021.py
+++ regular_bites/bite021.py
@@ -5,7 +5,7 @@ Given the provided cars dictionary:
 
     Get all Jeeps
     Get the first car of every manufacturer.
-    Get all vehicles containing the string Trail in their names (should work for other grep too)
+    Get all vehicles containing the string 't|Trail' in their names (should work for other grep too)
     Sort the models (values) alphabetically
 
 See the docstrings and tests for more details. Have fun!
--- regular_bites/test_bite021.py
+++ regular_bites/test_bite021.py
@@ -1,2 +1,24 @@
 import pytest
 import bite021
+
+
+CARS = bite021.cars
+
+def test_get_all_jeeps():
+    assert bite021.get_all_jeeps(CARS) == ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+    assert "Fairlane" not in bite021.get_all_jeeps(CARS)
+
+def test_get_first_model_each_manufacturer():
+    assert bite021.get_first_model_each_manufacturer() == ['Falcon', 'Commodore', "Maxima", "Civic", "Grand Cherokee"]
+
+def test_get_all_matching_models():
+    assert bite021.get_all_matching_models() == ['Trailblazer', 'Trailhawk']
+
+def test_sort_car_models():
+    assert bite021.sort_car_models() == {
+        'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+        'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+        'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+        'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+        'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+    }
