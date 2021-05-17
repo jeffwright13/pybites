f8585a61c4842f3082f801f00dcd0e652bb680b2
Jeff Wright <jeff.washcloth@gmail.com>
2021-05-17 07:53:34 -0400

    Working code, but needs refactoring.

--- regular_bites/bite026.py
+++ regular_bites/bite026.py
@@ -13,24 +13,30 @@ Bite 26. Dictionary comprehensions are awesome
     dict_variable = {key:value for (key,value) in dictonary.items()}
 
 """
-bites = {6: 'PyBites Die Hard',
-         7: 'Parsing dates from logs',
-         9: 'Palindromes',
-         10: 'Practice exceptions',
-         11: 'Enrich a class with dunder methods',
-         12: 'Write a user validation function',
-         13: 'Convert dict in namedtuple/json',
-         14: 'Generate a table of n sequences',
-         15: 'Enumerate 2 sequences',
-         16: 'Special PyBites date generator',
-         17: 'Form teams from a group of friends',
-         18: 'Find the most common word',
-         19: 'Write a simple property',
-         20: 'Write a context manager',
-         21: 'Query a nested data structure'}
+input_bites = {
+    6: "PyBites Die Hard",
+    7: "Parsing dates from logs",
+    9: "Palindromes",
+    10: "Practice exceptions",
+    11: "Enrich a class with dunder methods",
+    12: "Write a user validation function",
+    13: "Convert dict in namedtuple/json",
+    14: "Generate a table of n sequences",
+    15: "Enumerate 2 sequences",
+    16: "Special PyBites date generator",
+    17: "Form teams from a group of friends",
+    18: "Find the most common word",
+    19: "Write a simple property",
+    20: "Write a context manager",
+    21: "Query a nested data structure",
+}
 exclude_bites = {6, 10, 16, 18, 21}
 
 
-def filter_bites(bites=bites, bites_done=exclude_bites):
+def filter_bites(bites=input_bites, bites_done=exclude_bites):
     """return the bites dict with the exclude_bites filtered out"""
-    pass
+    ret_dict = {}
+    for item in bites.keys():
+        if item not in bites_done:
+            ret_dict[item] = bites[item]
+    return ret_dict
--- regular_bites/test_bite026.py
+++ regular_bites/test_bite026.py
@@ -1,1 +1,38 @@
 import bite026
+
+TEST_DICT = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+TEST_SET_1 = (1, 3, 5)
+TEST_SET_2 = ("a", "c", "e")
+TEST_SET_3 = ()
+TEST_SET_4 = set(TEST_DICT.keys())
+
+
+def test_input_set_not_empty_but_has_no_matching_items():
+    assert bite026.filter_bites(TEST_DICT, TEST_SET_1) == TEST_DICT
+
+
+def test_input_set_has_some_values_in_input_dict():
+    assert bite026.filter_bites(TEST_DICT, TEST_SET_2) == {"b": 2, "d": 4}
+
+
+def test_input_set_is_empty():
+    assert bite026.filter_bites(TEST_DICT, TEST_SET_3) == TEST_DICT
+
+
+def test_input_set_has_same_keys_as_input_dict():
+    assert bite026.filter_bites(TEST_DICT, TEST_SET_4) == {}
+
+
+def test_input_bites():
+    assert bite026.filter_bites(bite026.input_bites, bite026.exclude_bites) == {
+        7: "Parsing dates from logs",
+        9: "Palindromes",
+        11: "Enrich a class with dunder methods",
+        12: "Write a user validation function",
+        13: "Convert dict in namedtuple/json",
+        14: "Generate a table of n sequences",
+        15: "Enumerate 2 sequences",
+        17: "Form teams from a group of friends",
+        19: "Write a simple property",
+        20: "Write a context manager",
+    }
